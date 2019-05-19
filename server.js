91c5bc42a76f8d4e9ae8396e35c42cc2c0f4f7db
Barratt Isherwood <b4rr4tt@gmail.com>
2019-05-19 18:56:37 +0200

    feat(mail): mailer functionality

--- server.js
+++ server.js
@@ -1,8 +1,95 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  provider: 'gmail',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'info@shrikedigital.com', // Enter here email address from which you want to send emails
+    pass: 'wga38baL78fn' // Enter here password for email account from which you want to send emails
+  },
+  tls: {
+  rejectUnauthorized: false
+  }
+});
+
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.post('/send', function (req, res) {
+
+  let senderName = req.body.contactFormName;
+  let senderEmail = req.body.contactFormEmail;
+  let messageSubject = 'New contact form inquiry from ' + req.body.contactFormEmail;
+  let messageText = 'Please contact ' + req.body.contactFormEmail;
+  let copyToSender = req.body.contactFormCopy;
+
+  let mailOptions = {
+    to: ['info@shrikedigital.com'], // Enter here the email address on which you want to send emails from your customers
+    from: senderName,
+    subject: messageSubject,
+    text: messageText,
+    replyTo: senderEmail
+  };
+
+  if (senderName === '') {
+    res.status(400);
+    res.send({
+    message: 'Bad request'
+    });
+    return;
+  }
+
+  if (senderEmail === '') {
+    res.status(400);
+    res.send({
+    message: 'Bad request'
+    });
+    return;
+  }
+
+  if (messageSubject === '') {
+    res.status(400);
+    res.send({
+    message: 'Bad request'
+    });
+    return;
+  }
+
+  if (messageText === '') {
+    res.status(400);
+    res.send({
+    message: 'Bad request'
+    });
+    return;
+  }
+
+  if (copyToSender) {
+    mailOptions.to.push(senderEmail);
+  }
+
+  transporter.sendMail(mailOptions, function (error, response) {
+    if (error) {
+      console.log(error);
+      res.end('error');
+    } else {
+      console.log('Message sent: ', response);
+      res.end('sent');
+    }
+  });
+});
 
 app.use(express.static(__dirname + '/dist'));
 
--- src/app/services/connection.service.ts
+++ src/app/services/connection.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConnectionService {
-  url = 'http://localhost:3000/send';
+  url = 'http://localhost:8080/send';
   constructor(private http: HttpClient) { }
 
   sendMessage(messageContent: any) {
